beca5f20bdbd5bd8f41bc47dbe8454761f8a6563
JohannesFriedrich <Johannes.Friedrich@uni-bayreuth.de>
2017-11-27 22:03:09 +0100

    also add skip_on_travis() ...

--- tests/testthat/test_get_senseBox_data.R
+++ tests/testthat/test_get_senseBox_data.R
@@ -2,6 +2,7 @@ context("Test get_senseBox_data()")
 
 test_that("Check output length and class", {
   testthat::skip_on_cran()
+  testthat::skip_on_travis()
 
   temp <-  get_senseBox_data(
     senseBoxId = "5957b67494f0520011304cc1",
