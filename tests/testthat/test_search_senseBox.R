ad987d458ca86be120b689daf0e1d323885b1a56
JohannesFriedrich <johannes.friedrich@posteo.de>
2019-01-26 18:24:49 +0100

    modify test for search_sensebox to pass tests

--- tests/testthat/test_search_senseBox.R
+++ tests/testthat/test_search_senseBox.R
@@ -7,7 +7,7 @@ test_that("Check output length and class", {
   temp <-  search_senseBox(phenomenon = "Helligkeit",
                            date = "2018-01-01 00:00:00")
 
-  expect_equal(nrow(temp), 2)
+  expect_equal(nrow(temp), 3)
   expect_equal(class(temp), "data.frame")
 
   indoor_date <- search_senseBox(exposure = "indoor",
